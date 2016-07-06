f77b3a8301454c7a180cecbc3b8888523d31077d
Rafael Avila <rafa.avim@gmail.com>
2016-07-06 18:09:23 -0500

    add first request generator test, it failing ;)

--- test/requestGenerator.tests.js
+++ test/requestGenerator.tests.js
@@ -1,4 +1,24 @@
+import requestGenerator from '../lib/requestGenerator'
+
 describe('requestGenerator', () => {
-  it('should pass', () => {
+  let req
+
+  before(() => {
+    expect(requestGenerator).to.be.a.Object
+    expect(requestGenerator.generate).to.be.a.Funtion
+
+    // left: entity, verb, headers, qs, ops = {}
+    req = requestGenerator.generate(global.BASE_URL, global.QBO_CONSUMER_KEY, global.QBO_CONSUMER_SECRETE, global.TOKEN, global.TOKEN_SECRET, global.REALM_ID)
+  })
+
+  it('should request Company information', (done) => {
+    const uri = `companyinfo/${global.REALM_ID}`
+    req('companyinfo', 'GET', {}, {}, {uri})
+      .then((data) => {
+        expect(data).to.not.be.null
+        expect(data).to.be.a.Object
+        done()
+      })
+      .catch(done)
   })
 })
