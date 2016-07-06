a4412cf53e4e77dba1c0f71a326c20174b08be04
Rafael Avila <rafa.avim@gmail.com>
2016-07-06 16:11:52 -0500

    tests infreschurchur set up

---
+++ test/requires.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+global.expect = require('chai').expect
+
+global.QBO_CONSUMER_KEY = 'qyprdUzGWSCOgeetxJHPmB4uMAuLyY'
+global.QBO_CONSUMER_SECRETE = 'wMQ5CZFjSK4jEcZLYnVKM4dlycC3vZSjLihzq4L1'
+global.TOKEN_SECRET = 'kfgWWRtnMaOUasjdU6j3TI4vDaQrxd3anwdNcrKe'
+global.TOKEN = 'qyprd8eUIj3MTE3XahYwHwnXWbq9GFfqiHN9Vi1VyFfL3r6k'
+global.REALM_ID = '193514324492967'
