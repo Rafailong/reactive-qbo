b969f99190a6d09a24532ff6a13702deb9a344f6
Rafael Avila <rafa.avim@gmail.com>
2016-07-22 14:07:09 -0500

    Update requires.js
    delete QBO OAuth keys

--- test/requires.js
+++ test/requires.js
@@ -1,8 +1,8 @@
 global.expect = require('chai').expect
 
 global.BASE_URL = 'https://sandbox-quickbooks.api.intuit.com/v3/company/'
-global.QBO_CONSUMER_KEY = 'qyprdUzGWSCOgeetxJHPmB4uMAuLyY'
-global.QBO_CONSUMER_SECRETE = 'wMQ5CZFjSK4jEcZLYnVKM4dlycC3vZSjLihzq4L1'
-global.TOKEN = 'qyprdVXxXycFYC3UG2sCCE0ZCdpL9LLcTc0mlBRGwLjPl5tc'
-global.TOKEN_SECRET = 'RrZSipVAUw2OS3zPwz6ozYtLKMHz1Y8kvrNy95Zr'
-global.REALM_ID = '123145726091242'
+global.QBO_CONSUMER_KEY = ''
+global.QBO_CONSUMER_SECRETE = ''
+global.TOKEN = ''
+global.TOKEN_SECRET = ''
+global.REALM_ID = ''
